418bdf6df0ad802750d1025347ddd4a4093577a6
amitKotwariya <amitkotwariya@policybazaar.com>
2021-08-23 16:33:12 +0530

    changes title to Akhari Vaar

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -24,7 +24,7 @@ const NavBar = () => {
 	return (
 		<>
 			<nav className={classes["main-nav"]}>
-				<h3 className={classes["logo"]}>Teen Patti</h3>
+				<h3 className={classes["logo"]}>Akhari Var</h3>
 
 				<ul>
 					{isLoggedIn && (
